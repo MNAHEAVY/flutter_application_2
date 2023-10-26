d96465e73cd1aa158b0f9f9894f036b141f8a9c7
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2023-10-25 21:55:33 -0300

    apk added

---
+++ lib/screens/splash.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_2/main.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+// void main() {
+//   runApp(const MyApp());
+//   FlutterNativeSplash.show(
+//     title: 'My App', // Título de la aplicación
+//     theme: ThemeData.dark(), // Tema de la pantalla de presentación
+//     splashImage: 'assets/tu_imagen.png', // Ruta de la imagen
+//     useLoader: false, // Utilizar un indicador de carga personalizado
+//   );
+// }
+
+// class FlutterNativeSplash {
+//   static void show({required String title, required ThemeData theme, required String splashImage, required bool useLoader}) {}
+// }
